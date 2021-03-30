553db43147edf098e7b35caac01aae43d62c5fd8
SInjomin <a>
2021-03-31 00:02:08 +0300

    Add canvas filling

--- Graphs/README.md
+++ Graphs/README.md
@@ -1,9 +1,15 @@
 # Graphs
 
+About
+----------
+
+Graphs is opensource program in which you can
+create Graph and save it to the svg file.
+
 Starting
 ----------
-Install dependencies uing
-'npm install'
-
-Start the programm
-'npm start'
+* Install dependencies using  
+  `npm install`
+  
+* Start the program \
+  `npm start`
--- Graphs/js/canvas.js
+++ Graphs/js/canvas.js
@@ -3,5 +3,20 @@ var ctx;
 
 function Init() {
     canvas = document.getElementById("cnvs");
+    canvas.Width = 420 //Ширина холста
+    canvas.Height = 320 //Высота холста
     ctx = canvas.getContext("2d");
+    Draw();
+}
+
+function Draw() {
+    ctx.fillStyle = "rgb(255,255,255)"
+    ctx.fillRect(0, 0, canvas.Width, canvas.Height);
+    ctx.fill();
+}
+
+canvas.onclick = function (e) {
+    var x = (e.pageX - canvas.offsetLeft) / cellSize | 0;
+    var y = (e.pageY - canvas.offsetTop)  / cellSize | 0;
+    event(x, y); // выхов функции действия
 }
